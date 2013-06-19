4e72f1f2b763fd089d0fdcc0c2fbc49c58a3609d
Rafael B. Tauil <rafael@tauil.com.br>
2013-06-18 22:54:03 -0300

    Fixes mass-assign error

--- app/models/spree/wished_product.rb
+++ app/models/spree/wished_product.rb
@@ -4,5 +4,5 @@ class Spree::WishedProduct < ActiveRecord::Base
 
   scope :active, includes(:variant).where("spree_variants.deleted_at IS NULL")
 
-  attr_accessible :variant_id
+  attr_accessible :variant_id, :wishlist_id
 end
